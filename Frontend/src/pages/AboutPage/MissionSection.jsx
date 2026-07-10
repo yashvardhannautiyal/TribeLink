ed429ab6cd2c1c212b47913ef58df505f6e1fb5a
TusharBhardwaj18 <bhardwajt161@gmail.com>
2026-07-11 01:34:53 +0530

    created HeroSection

--- Frontend/src/pages/AboutPage/MissionSection.jsx
+++ Frontend/src/pages/AboutPage/MissionSection.jsx
@@ -31,46 +31,29 @@ const stats = [
 const MissionSection = () => {
   return (
     <section className="bg-[#17171f] py-24 ">
-
       <div className="max-w-6xl mx-auto px-6">
-
         <div className="grid lg:grid-cols-2 gap-16 items-center">
-
           {/* Left */}
-
           <div>
-
             <p className="text-orange-500 uppercase tracking-[3px] font-bold -mt-33 text-sm">
               OUR MISSION
             </p>
-
             <h2 className="text-3xl lg:text-3xl font-bold mt-5">
               Connecting the World's Players
             </h2>
-            
             <p className="mt-6 text-gray-400 leading-7 text-lg">
-
               We started TribeLink in 2026 with one goal: eliminate the
               loneliness of gaming and sport. Too many players sit at home unable to find opponents, or show up to a venue with no one to play against.
-
             </p>
-
             <p className="mt-6 text-gray-400 leading-7 text-lg">
-
               Today, over 6,500 players across the UK use TribeLink to
               find matches, build rivalries and form lasting
               communities - all centred around the games and sports they love.
-
             </p>
-
           </div>
-
           {/* Right */}
-
-          <div className="grid sm:grid-cols-2 gap-3 -mt-20">
-
+          <div className="grid sm:grid-cols-2 gap-3 -mt-10">
             {stats.map((item, index) => (
-
               <div
                 key={index}
                 className="bg-[#0f0f16]
@@ -81,29 +64,17 @@ const MissionSection = () => {
                 hover:-translate-y-2
                 duration-300"
               >
-
                 <h3 className={`text-3xl font-bold ${item.color}`}>
-
                   {item.number}
-
                 </h3>
-
                 <p className="mt-1 text-gray-400 tracking-[4px]">
-
                   {item.title}
-
                 </p>
-
               </div>
-
             ))}
-
           </div>
-
         </div>
-
       </div>
-
     </section>
   );
 };
--- Frontend/src/pages/HomePage/HeroSection.jsx
+++ Frontend/src/pages/HomePage/HeroSection.jsx
@@ -1,11 +1,147 @@
 import React from 'react'
+import { ArrowRight, Star ,Zap ,ChevronRight } from "lucide-react";
+
+const players = [
+  {
+    initials: "M",
+    name: "Marcus",
+    game: "PS5 Gaming",
+    rating: "4.9",
+    badge: "PRO",
+    color: "bg-red-600",
+    position: "top-10 left-5",
+  },
+  {
+    initials: "Y",
+    name: "Yash",
+    game: "Football",
+    rating: "4.7",
+    badge: "PRO",
+    color: "bg-blue-600",
+    position: "top-32 right-6",
+  },
+  {
+    initials: "D",
+    name: "Dev",
+    game: "Pool",
+    rating: "4.5",
+    badge: "ADVANCED",
+    color: "bg-purple-600",
+    position: "bottom-49 left-12",
+  },
+  {
+    initials: "T",
+    name: "Tushar",
+    game: "Bowling",
+    rating: "4.6",
+    badge: "ADVANCED",
+    color: "bg-orange-500",
+    position: "bottom-25 right-6",
+  },
+];
+
 
 
 function HeroSection() {
   return (
-    <div>
-      this is hero section
-    </div>
+    <section className="relative overflow-hidden bg-[#09090f] text-white ">
+    <div  className="absolute inset-0 opacity-10
+        bg-[linear-gradient(rgba(255,255,255,.15)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,.15)_1px,transparent_1px)]
+        bg-[size:72px_72px]" />
+      <div className="absolute top-20 right-40 h-80 w-80 rounded-full bg-orange-500/10 blur-[120px]" />
+      <div className="absolute bottom-20 left-1/2 h-72 w-72 rounded-full bg-blue-500/10 blur-[120px]" />
+          <div  className="relative max-w-7xl mx-auto px-6 lg:px-8">
+            <div className="grid lg:grid-cols-2 gap-16 items-center min-h-[92vh]">
+              {/* left */}
+              <div>
+                <span className='inline-flex items-center gap-2 border mt-4 ml-2 border-orange-500/70 bg-orange-500/30 hover:bg-orange-500/40 text-orange-400 rounded-full px-5 py-2 text-xs tracking-[3px] uppercase font-semibold'>
+                <Zap size={15} /> The Player Network
+                </span>
+                <h1 className="mt-4 font-black leading-none">
+                  <span className="block text-5xl sm:text-6xl lg:text-6xl">Link Your Tribe</span>
+                 <span className="block mt-2 text-5xl sm:text-6xl lg:text-6xl text-orange-500 ">
+                Tribe.
+              </span>
+              <span className="block mt-2 text-5xl sm:text-6xl lg:text-6xl bg-gradient-to-r from-red-500 via-blue-700 bg-clip-text text-transparent">
+                Dominate
+              </span>
+              <span className="block mt-2 text-5xl sm:text-6xl lg:text-6xl">
+                Together.
+              </span>
+              </h1>
+               <p className=" mt-8 text-gray-400 text-lg max-w-xl leading-7.5">
+              Connect with players who share your passion. Organise
+              matches, track your performance, and rise through the
+              ranks — PS5, Football, Pool, Bowling and more.
+            </p>
+             <div className="mt-10 flex flex-col sm:flex-row gap-5">
+                <button className="border bg-orange-600/20 border-orange-500 hover:bg-orange-600 transition px-8 py-4 rounded-xl font-bold flex items-center justify-center gap-2">
+                JOIN NOW
+                <ArrowRight size={20} />
+              </button>
+               <button className="border border-blue-600 text-blue-500 hover:bg-blue-600 hover:text-white transition px-8 py-4 rounded-xl font-bold flex items-center justify-center gap-2">
+                Explore
+                <ChevronRight size={20} />
+              </button>
+             </div>
+               </div>
+          <div className="relative hidden lg:block md:block md:h-[460px] h-[580px]">
+
+            {players.map((player, index) => (
+              <div
+                key={index}
+                className={`absolute ${player.position} w-50 rounded-2xl border border-white/10 bg-[#11111b]/90 backdrop-blur-md p-3 shadow-xl`}
+              >
+                <div className="flex items-center gap-4">
+
+                  <div
+                    className={`w-12 h-12 rounded-full ${player.color} flex items-center justify-center font-bold`}
+                  >
+                    {player.initials}
+                  </div>
+
+                  <div> 
+                    <h3 className="font-semibold text-lg">
+                      {player.name}
+                    </h3>
+
+                    <p className="text-gray-400 text-sm">
+                      {player.game}
+                    </p>
+                  </div>
+
+                </div>
+
+                <div className="flex items-center mt-2 gap-1">
+
+                  {[...Array(5)].map((_, i) => (
+                    <Star
+                      key={i}
+                      size={15}
+                      fill="#FACC15"
+                      stroke="#FACC15"
+                    />
+                  ))}
+
+                  <span className="ml-1 text-gray-400">
+                    {player.rating}
+                  </span>
+
+                  <span className="ml-auto text-[7px] tracking-widest bg-red-900/30 text-red-600 border border-red-500/30 rounded-full px-3 py-1">
+                    {player.badge}
+                  </span>
+
+                </div>
+
+              </div>
+            ))}
+
+          </div>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
