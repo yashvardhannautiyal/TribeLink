25ddeb332145e6e56a4284cab1128050b1139afb
TusharBhardwaj18 <bhardwajt161@gmail.com>
2026-08-17 23:52:38 +0530

    created Process.jsx

--- Frontend/src/pages/HomePage/HomePage.jsx
+++ Frontend/src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import LeaderBoard from './LeaderBoard'
 import UserLogComp from './UserLogComp'
+import Process from './Process'
 
 function HomePage() {
   return (
     <div>
       <HeroSection />
       <LeaderBoard />
+      <Process />
       <UserLogComp />
     </div>
   )
